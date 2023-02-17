edd6f0c569128db2d9de0de89f17cec5b9d0c06c
Christina <stinacon@gmail.com>
2023-02-17 13:28:08 -0500

    import random; random.choice() successfully prints a random word from the list

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,8 @@
 # FUNCTION FOR OPENING FILE AND READING IT
 # defining the function w/o arguments because it's localized
+import random
+
+
 def open_file(file):
     # opening the file in read mode and will auto close it too
     with open(file) as my_file:
@@ -7,6 +10,8 @@ def open_file(file):
         data = my_file.read()
         format_data = data.split()
         return format_data
+
+# def choose_random_word() --> choice() from a random module? or from my .txt file
 
 
 # print(open_file('words.txt'))
@@ -17,9 +22,13 @@ def open_file(file):
 
 def play_game():
     word_list = open_file('words.txt')
-    print(word_list)
+    word_to_guess = random.choice(word_list)
+    print(word_to_guess)
     # word = 'apple'
-    # user_guess = input('Guess a lowercase letter! ')
+    user_guess = input('Guess a lowercase letter! ')
+    # ? need to create empty lists (or something) to store user guesses, wrong
+    # and right guesses - will need to store a count of one or both of those
+
     # wrong_guesses = ['b', 'd']
     # right_guesses = ['a', 'p']
 
