70814ffd100c58d8f655354be92709bdfa9886e5
Christina <stinacon@gmail.com>
2023-02-18 08:33:07 -0500

    listed key of variable names for me to reference in notes.txt file

--- mystery_word.py
+++ mystery_word.py
@@ -1,33 +1,46 @@
-# FUNCTION FOR OPENING FILE AND READING IT
-# defining the function w/o arguments because it's localized
+
 import random
 
 
 def open_file(file):
-    # opening the file in read mode and will auto close it too
     with open(file) as my_file:
-        # reading the file on the right then in data it makes it readable for humans
         data = my_file.read()
         format_data = data.split()
         return format_data
 
 
+def choose_random_word(list):
+    word = random.choice(list)
+    return word
+
+
 def play_game():
-    word_list = open_file('words.txt')
-    word_to_guess = random.choice(word_list)
-    underscore = '_ ' * (len(word_to_guess))
-    # word_display = (f'({len(word_to_guess)}) * "_ "')
+    # good illustration of calling a function within a function
+    word_to_guess = choose_random_word(open_file('words.txt'))
+    word_length = len(word_to_guess)
+    underscore = '_ ' * word_length
+    underscore_as_list = list(underscore)
+
+    # for me to know what the word is for now; will take out later
     print(word_to_guess)
+    print(f'Your word has {word_length} letters.')
     print(underscore)
 
-    # word = 'apple'
-    user_guess = input('Guess a lowercase letter! ')
+    user_guess = (input('Guess a letter! ')).lower()
+    print(user_guess)
+    if len(user_guess) > 1:
+        user_guess = input("Your entry is invalid. Try again. ")
+        print(user_guess)
+    right_guesses = []
+    wrong_guesses = []
+    wrong_guess_count = len(wrong_guesses)
     # ? need to create empty lists (or something) to store user guesses, wrong
     # and right guesses - will need to store a count of one or both of those
 
+    # CLASS EXAMPLE
+    # word = 'apple'
     # wrong_guesses = ['b', 'd']
     # right_guesses = ['a', 'p']
-
     # game_board = ''
     # for letter in word:
     #     if letter in right_guesses:
