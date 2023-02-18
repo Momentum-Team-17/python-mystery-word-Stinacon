30316493de73d79ca9888bf65c7e5de2039476ca
Christina <stinacon@gmail.com>
2023-02-18 14:44:31 -0500

    displays updated game board if first guess is correct

--- mystery_word.py
+++ mystery_word.py
@@ -22,31 +22,30 @@ def play_game():
     # good illustration of calling a function within a function
     word_to_guess = choose_random_word(open_file('test-word.txt'))
     word_length = len(word_to_guess)
-    underscore = '_ ' * word_length
+    underscore = '_' * word_length
     # underscore_as_list = list(underscore)
 
     # for me to know what the word is for now; will take out later
     print(word_to_guess)
     print(f'Your word has {word_length} letters.')
     print(underscore)
+    # add spaces to this print statement
 
-    user_guess = (input('Guess a letter! ')).lower()
-    print(user_guess)
+    user_guess = input('Guess a letter!\n').lower()
     if len(user_guess) > 1:
         user_guess = input("Your entry is invalid. Try again. ")
-        print(user_guess)
+        # print(user_guess)
 
-    right_guess_count = 0
+    wrong_guess_letters = []
     wrong_guess_count = 0
-
-    i = 0
-    for char in word_to_guess:
-        current_index = word_to_guess[i]
-        i += 1
-        if current_index == user_guess:
-            updated_game_board = underscore[:i] + \
-                user_guess + underscore[i+1:]
-            print(updated_game_board)
+    underscore_list = list(underscore)
+    # The below loop tests one guess against the word; prints updated gameboard if guess is correct.
+    for index in range(len(word_to_guess)):
+        if word_to_guess[index] == user_guess:
+            underscore_list[index] = user_guess
+            print("You guessed right!")
+            print(' '.join(underscore_list))
+            # m in dream is not printing correctly
 
 # while loop: while wrong_guess_count < 8...
 # break: when wrong_guess_count = 8
