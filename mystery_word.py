103038500e408c18dca6438f879ca1a6c4546d32
Christina <stinacon@gmail.com>
2023-02-18 09:21:50 -0500

    able to iterate through word_to_guess and eval if guess is correct; still unable to print string with replaced underscore

--- mystery_word.py
+++ mystery_word.py
@@ -14,12 +14,16 @@ def choose_random_word(list):
     return word
 
 
+# def check_one_guess(word):
+#     for char in word:
+
+
 def play_game():
     # good illustration of calling a function within a function
-    word_to_guess = choose_random_word(open_file('words.txt'))
+    word_to_guess = choose_random_word(open_file('test-word.txt'))
     word_length = len(word_to_guess)
     underscore = '_ ' * word_length
-    underscore_as_list = list(underscore)
+    # underscore_as_list = list(underscore)
 
     # for me to know what the word is for now; will take out later
     print(word_to_guess)
@@ -31,11 +35,23 @@ def play_game():
     if len(user_guess) > 1:
         user_guess = input("Your entry is invalid. Try again. ")
         print(user_guess)
-    right_guesses = []
-    wrong_guesses = []
-    wrong_guess_count = len(wrong_guesses)
-    # ? need to create empty lists (or something) to store user guesses, wrong
-    # and right guesses - will need to store a count of one or both of those
+
+    right_guess_count = 0
+    wrong_guess_count = 0
+    i = 0
+    for char in word_to_guess:
+        current_index = word_to_guess[i]
+        i += 1
+        if current_index == user_guess:
+            print("Nice guess")
+
+    # while loop: while wrong_guess_count < 8...
+    # break: when wrong_guess_count = 8
+
+    # trying to write code for process of just one guess:
+    # for char in s:
+    # print(char)
+
 
     # CLASS EXAMPLE
     # word = 'apple'
@@ -48,7 +64,5 @@ def play_game():
     #     else:
     #         game_board += '_ '
     # print(game_board)
-
-
 if __name__ == "__main__":
     play_game()
